980b55c2dac42b7bf13aaba47cb7b3f5111dec19
Sergey Gunin <sgunin@rambler.ru>
2025-01-21 00:01:34 +0300

    Add SDK documentation

---
+++ Documentation/HikRobot/SDK_Docs/html/menudata.js
@@ -0,0 +1,42 @@
+/*
+@ @licstart  The following is the entire license notice for the
+JavaScript code in this file.
+
+Copyright (C) 1997-2017 by Dimitri van Heesch
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+@licend  The above is the entire license notice
+for the JavaScript code in this file
+*/
+var menudata={children:[
+{text:"Home Page",url:"index.html"},
+{text:"Release Notes",url:"_xE5_x8F_x91_xE7_x89_x88_xE8_xAF_xB4_xE6_x98_x8E.html"},
+{text:"Environment Configuration",url:"_xE7_x8E_xAF_xE5_xA2_x83_xE9_x85_x8D_xE7_xBD_xAE.html"},
+{text:"Programming Guide",url:"_xE7_xBC_x96_xE7_xA8_x8B_xE5_xBC_x95_xE5_xAF_xBC.html",children:[
+{text:"Development Environment Configuration",url:"_xE9_x85_x8D_xE7_xBD_xAE_xE5_xBC_x80_xE5_x8F_x91_xE7_x8E_xAF_xE5_xA2_x83.html"},
+{text:"Before You Start",url:"_xE5_xBC_x80_xE5_x8F_x91_xE5_x89_x8D_xE5_xBF_x85_xE8_xAF_xBB.html"},
+{text:"SDK Initialization",url:"_xE5_x88_x9D_xE5_xA7_x8B_xE5_x8C_x96_s_d_k.html"},
+{text:"Camera Initialization",url:"_xE5_x88_x9D_xE5_xA7_x8B_xE5_x8C_x96_xE7_x9B_xB8_xE6_x9C_xBA.html"},
+{text:"Frame Grabber Initialization",url:"_xE5_x88_x9D_xE5_xA7_x8B_xE5_x8C_x96_xE9_x87_x87_xE9_x9B_x86_xE5_x8D_xA1.html"},
+{text:"Parameter Configuration",url:"_xE8_xAE_xBE_xE7_xBD_xAE_xE5_x8F_x82_xE6_x95_xB0.html"},
+{text:"Event and Exception",url:"_xE4_xBA_x8B_xE4_xBB_xB6_xE5_x8F_x8A_xE5_xBC_x82_xE5_xB8_xB8.html"},
+{text:"Image Acquisition",url:"_xE9_x87_x87_xE9_x9B_x86_xE5_x9B_xBE_xE5_x83_x8F.html"},
+{text:"Image Processing",url:"_xE5_xA4_x84_xE7_x90_x86_xE5_x9B_xBE_xE5_x83_x8F.html"}]},
+{text:"API Reference",url:"modules.html"},
+{text:"Camera Parameter Node Table",url:"_xE7_x9B_xB8_xE6_x9C_xBA_xE5_x8F_x82_xE6_x95_xB0_xE8_x8A_x82_xE7_x82_xB9_xE8_xA1_xA8.html"},
+{text:"Sample Program Overview",url:"_xE7_xA4_xBA_xE4_xBE_x8B_xE7_xA8_x8B_xE5_xBA_x8F_xE6_xA6_x82_xE8_xA7_x88.html"},
+{text:"FAQs",url:"_xE5_xB8_xB8_xE8_xA7_x81_xE9_x97_xAE_xE9_xA2_x98.html"},
+{text:"Disclaimer",url:"_xE6_xB3_x95_xE5_xBE_x8B_xE5_xA3_xB0_xE6_x98_x8E.html"}]}
